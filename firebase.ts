d7feaa9a267b1d7a4fd7538721f74b67f7dcd716
benjaminfynn-glitch <benjaminfynn@gmail.com>
2025-12-01 20:55:00 +0000

    Update Firebase config

--- firebase.ts
+++ firebase.ts
@@ -4,20 +4,17 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuG16WHQCFJ1cQR9dpPFwrSWFLQk6cLE",
+  apiKey: "AIzaSyDuG16WHQCFJ1cQR9dpPFwrSWFLQkc6LE",
   authDomain: "church-konet.firebaseapp.com",
   projectId: "church-konet",
-  storageBucket: "church-konet.appspot.com",
+  storageBucket: "church-konet.firebasestorage.app",
   messagingSenderId: "317305373118",
-
-  // IMPORTANT: Replace this with the REAL APP ID from Firebase console
-  appId: "1:317305373118:web:5c8c7e66a3d0ffa367b9ef",
-
+  appId: "1:317305373118:web:5c8c7e66a30dffa367b9ef",
   measurementId: "G-F7TYLTETG3",
 };
 
 // Prevent double initialization
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
